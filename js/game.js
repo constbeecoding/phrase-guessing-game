d02395ce449f0934b082cd736b4d285b265503ce
Bee Kay <brittkcrouch@gmail.com>
2021-01-09 18:31:09 -0600

    Added variables, array, and some function stubs

--- js/game.js
+++ js/game.js
@@ -1,3 +1,33 @@
-let qwerty = ;
-let phrase = ;
-let variable = ;
+// VARIABLES//
+
+const qwerty = document.getElementById('querty');
+const phrase = document.getElementById('phrase');
+const buttonReset = document.getElementByClassName('.btn_reset');
+
+const missed = ('0')
+
+//PHRASES ARRAY//
+
+const phrasesArray = arr => {
+    ['Id just as soon kiss a Wookiee'],
+    ['Help me ObiWan Kenobi'],
+    ['May the force be with you'],
+    ['I find your lack of faith distrubing'],
+    ['Scruffy looking nerf herder']
+}
+
+//START GAME -- BUTTON FUNCTION//
+
+function startGame = ;
+
+//RETURN RANDOM PHRASE FROM ARRAY//
+
+//ADD THE LETTERS OF A STRING TO THE DISPLAY//
+
+//CHECK IF A LETTER IS IN THE PHRASE//
+
+//CHECK IF THE GAME HAS BEEN WON OR LOST//
+
+//LISTEN FOR THE START GAME BUTTON TO BE PRESSED//
+
+//LISTEN FOR THE ONSCREEN KEYBOARD TO BE CLICKED//
